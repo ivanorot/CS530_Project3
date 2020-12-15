cf890655a229a49240b113016f297d03ab09ac6b
Ivan Orozco <ivan.orot93@gmail.com>
2020-12-14 23:12:24 -0800

    less gooo

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -118,7 +118,7 @@ bool LexicalAnalyzer::checkIfId(string id) {
 }
 
 bool LexicalAnalyzer::checkIfChar(int ascii) {
-    if (ascii < 48 || (ascii > 57 && ascii < 97) || (ascii > 122)) {
+    if (ascii < 65 || (ascii > 90 && ascii < 97) || (ascii > 122)) {
         return false;
     }
     return true;
